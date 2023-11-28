8aba92773a99158202e3f1a9613ffc63e44ff7b9
reservoirinvest <reservoirinvest@gmail.com>
2023-11-28 09:51:38 +0800

    modified with from-root

--- src/snp.py
+++ src/snp.py
@@ -1,6 +1,117 @@
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
+import yaml
+from ib_insync import IB
+from loguru import logger
 
-dls = "http://www.cboe.com/products/weeklys-options/available-weeklys"
-data = pd.read_html(dls)
+
+def read_weeklys() -> pd.DataFrame:
+    """gets weekly cboe symbols"""
+
+    dls = "http://www.cboe.com/products/weeklys-options/available-weeklys"
+    df = pd.read_html(dls)[0]
+
+    return df
 
 
+def rename_weekly_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """standardizes column names of cboe"""
+    
+    df.columns=['desc', 'symbol']
+
+    return df
+
+
+def remove_non_char_symbols(df: pd.DataFrame) -> pd.DataFrame:
+    """removes symbols with non-chars - like dots (BRK.B)"""
+    
+    df = df[df.symbol.str.extract("([^a-zA-Z])").isna()[0]]
+
+    return df
+
+
+
+def make_weekly_cboes() -> pd.DataFrame:
+    """
+    Generates a weekly cboe symbols dataframe
+    """
+
+    df = (
+        read_weeklys()
+        .pipe(rename_weekly_columns)
+        .pipe(remove_non_char_symbols)
+        )
+    
+    return df
+
+
+def get_snps() -> pd.Series:
+    """
+    gets snp symbols from wikipedia
+    """
+    snp_url =  "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    snps = pd.read_html(snp_url)[0]['Symbol']
+    return snps
+
+
+def add_indexes(df: pd.DataFrame, path_to_yaml_file: str) -> pd.DataFrame:
+    """
+    add indexes from config/snp_indexes.yaml
+    """
+    with open(path_to_yaml_file, 'r') as f:
+        kv_pairs = yaml.load(f, Loader=yaml.FullLoader)
+
+    more_df = pd.DataFrame(list(kv_pairs.items()), columns=['symbol', 'desc'])
+
+    df_all = pd.concat([df, more_df], ignore_index=True)
+    
+    return df_all
+
+
+def split_stocks_and_index(df: pd.DataFrame) -> pd.DataFrame:
+    """differentiates stocks and index"""
+    
+    df = df.assign(secType=np.where(df.desc.str.contains('Index'), 'IND', 'STK'))
+
+    return df
+
+
+def make_snp_weeklies(indexes_path: Path):
+    """
+    Makes snp weeklies with indexes
+    """
+
+    # get snp stock weeklies
+    df_weekly_cboes = make_weekly_cboes()
+    snps = get_snps()
+
+    # filter weekly snps
+    df_weekly_snps = df_weekly_cboes[df_weekly_cboes.symbol.isin(snps)] \
+                    .reset_index(drop=True)
+
+    # add index weeklies
+    df_weeklies = (
+        add_indexes(df_weekly_snps, indexes_path)
+        .pipe(split_stocks_and_index)
+        )
+    
+    return df_weeklies
+
+
+
+if __name__ == "__main__":
+
+    from utils import get_project_root
+
+    root = get_project_root()
+
+    indexes_path = root / 'data' / 'master' / 'snp_indexes.yml'
+    
+    df = make_snp_weeklies(indexes_path)
+    
+    logger.info(df)
+
+
+    
---
+++ src/utils.py
@@ -0,0 +1,57 @@
+import datetime
+from dataclasses import dataclass
+from pathlib import Path
+from from_root import from_root
+
+from loguru import logger
+
+
+@dataclass
+class Timediff:
+    """Stores time difference"""
+    days: int
+    hours: int
+    minutes: int
+    seconds: float
+
+
+def get_file_age(file_path: Path):
+    """Gets age of a file"""
+
+    time_now = datetime.datetime.now()
+
+    try:
+
+        file_time = datetime.datetime.fromtimestamp(file_path.stat().st_mtime)
+
+    except FileNotFoundError as e:
+
+        logger.info(f"File not found in {str(file_path)}")
+        
+        file_age = None
+
+    else:
+
+        # convert time difference to days, hours, minutes, secs
+        td = (time_now - file_time)
+        days = td.days
+        hours, remainder = divmod(td.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        seconds += td.microseconds / 1e6
+
+        file_age = Timediff(*(days, hours, minutes, seconds))
+
+    return file_age
+
+
+if __name__ == "__main__":
+
+    from loguru import logger
+
+    ROOT = from_root()
+    file_path = ROOT / 'data' / 'master' / 'snp_indexes.yml'
+
+    td = get_file_age(file_path)
+
+    logger.info(td)
+
